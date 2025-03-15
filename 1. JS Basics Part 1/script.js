d08a17032994e473d0f8ea076d2acc64757ffe4b
Muhammad Farhan <farhan_75230@hotmail.com>
2025-03-15 10:22:20 +0100

    four exercises completed!

---
+++ 1. JS Basics Part 1/script.js
@@ -0,0 +1,48 @@
+///////////////Exercise 1/////////////////
+/*let MyName = "Farhan";
+let age = 25;
+let isStudent = true;
+
+console.log(
+  "Name of Student is: " +
+    MyName +
+    ", the age is " +
+    age +
+    ", is he student: " +
+    isStudent
+);
+*/
+
+////////////Exercise 2//////////////
+/*let food = ["food1", "food2", "food3"];
+
+let person = {
+  name: "Farhan",
+  age: 35,
+  hobbies: ["skiing", "dancing", "running"],
+};
+
+console.log(person + " has the favourite foods " + food);
+*/
+
+////////////Exercise 3//////////////
+/*function age(alt) {
+  if (alt > 18) console.log("Adult");
+  else console.log("Minor");
+}
+
+age(15);
+
+
+//////////Exercise 4///////////
+/*for (let count = 1; count < 11; count++) {
+  console.log(count);
+}
+
+let count = 1;
+while (count < 11) {
+  console.log(count);
+  count++;
+}*/
+
+/////////Exercise 
