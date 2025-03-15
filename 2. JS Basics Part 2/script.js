8148620e723d9a9faf23d7130d719fe19953c4df
Muhammad Farhan <farhan_75230@hotmail.com>
2025-03-15 14:53:51 +0100

    array fixed.

--- 2. JS Basics Part 2/script.js
+++ 2. JS Basics Part 2/script.js
@@ -1,21 +1,24 @@
 // https://leetcode.com/
 // https://www.hackerrank.com/
 ///////////////////////Exercise 1///////////
-/*function FirstElements(arr, n) {
+function FirstElements(arr, n) {
+  let new_array = [];
+
   if (n > arr.length) {
     console.log("Inavlid Number specified");
     return "Exiting Program!";
   }
 
   for (let count = 0; count < n; count++) {
-    console.log(arr[count]);
+    // console.log(arr[count]);
+    new_array.push(arr[count]);
   }
+  console.log(new_array);
 }
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 FirstElements(arr, 3);
-*/
 
 ///////////////Exercise 2//////////////
 //https://www.geeksforgeeks.org/count-occurrences-of-all-items-in-an-array-in-javascript/
