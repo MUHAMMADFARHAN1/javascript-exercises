568fb7278f67382b9715129924ea4303f62dc4be
Muhammad Farhan <farhan_75230@hotmail.com>
2025-03-15 10:53:30 +0100

    Part 2 Exercise 1 completed

---
+++ 2. JS Basics Part 2/script.js
@@ -0,0 +1,18 @@
+///////////////////////Exercise 1///////////
+/*function FirstElements(arr, n) {
+  if (n > arr.length) {
+    console.log("Inavlid Number specified");
+    return "Exiting Program!";
+  }
+
+  for (let count = 0; count < n; count++) {
+    console.log(arr[count]);
+  }
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+FirstElements(arr, 3);
+*/
+
+///////////////Exercise 2//////////////
